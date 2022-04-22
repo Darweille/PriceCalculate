747fc8c975616a1a50fca6d6726b529fe3fe73d9
Darweille <wimitsuhide@gmail.com>
2022-04-22 20:49:06 +0800

    Add files via upload

---
+++ calculate.js
@@ -0,0 +1,19 @@
+﻿function Calculate()
+{
+	var Fee = 0.001425;
+	var Tax = 0.0015
+	
+	var Discount = Number(document.getElementById("Discount").value);
+	
+	var BuyPrice = Number(document.getElementById("BuyPrice").value);
+	var BuyFee = BuyPrice * (Fee * Discount );
+	
+	document.getElementById("BuyFee").value = BuyFee.toFixed(4);
+	
+	var SellPrice = Number(document.getElementById("SellPrice").value);
+	var SellFee = SellPrice * (Fee * Discount );
+	var SellTax = SellPrice * Tax;
+	
+	document.getElementById("SellFee").value = SellFee.toFixed(4);
+	document.getElementById("SellTax").value = SellTax.toFixed(4);
+}
