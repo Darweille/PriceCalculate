67512a1abd9f4a168136c2a2816a01789d1a2b43
Darweille <wimitsuhide@gmail.com>
2022-04-22 20:52:45 +0800

    Add files via upload

--- calculate.js
+++ calculate.js
@@ -8,12 +8,12 @@
 	var BuyPrice = Number(document.getElementById("BuyPrice").value);
 	var BuyFee = BuyPrice * (Fee * Discount );
 	
-	document.getElementById("BuyFee").value = BuyFee.toFixed(4);
+	document.getElementById("BuyFee").value = BuyFee.toFixed(1);
 	
 	var SellPrice = Number(document.getElementById("SellPrice").value);
 	var SellFee = SellPrice * (Fee * Discount );
 	var SellTax = SellPrice * Tax;
 	
-	document.getElementById("SellFee").value = SellFee.toFixed(4);
-	document.getElementById("SellTax").value = SellTax.toFixed(4);
+	document.getElementById("SellFee").value = SellFee.toFixed(1);
+	document.getElementById("SellTax").value = SellTax.toFixed(1);
 }
